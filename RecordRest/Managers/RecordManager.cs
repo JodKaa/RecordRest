59444eb124a87f972a61ad13c2f98f3c308af864
Jonas Kristensen <jona910a@edu.easj.dk>
2021-10-25 09:52:26 +0200

    Jonas1

---
+++ RecordRest/Managers/RecordManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RecordRest.Models;
+
+namespace RecordRest.Managers
+{
+    public class RecordManager
+    {
+        public static List<Record> records = new List<Record>
+        {
+            new Record {Artist = "Michael Jackson", Title = "Thriller", Duration = 360, PublicationYear = 1982},
+            new Record {Artist = "Kim Larsen", Title = "Det Bedste Til Mig og Mine Venner", Duration = 240, PublicationYear = 1977}
+        };
+        
+
+        public List<Record> GetAllRecords()
+        {
+            return records;
+        }
+    }
+}
---
+++ RecordRest/Models/Record.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RecordRest.Models
+{
+    public class Record
+    {
+        public string Title { get; set; }
+        public string Artist { get; set; }
+        public int Duration { get; set; }
+        public int PublicationYear { get; set; }
+
+        public Record(string title, string artist, int duration, int publicationYear)
+        {
+            Title = title;
+            Artist = artist;
+            Duration = duration;
+            PublicationYear = publicationYear;
+        }
+
+        public Record()
+        {
+        }
+    }
+}
